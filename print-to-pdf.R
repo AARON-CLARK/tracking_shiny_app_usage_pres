47efe8d5a8bb4fa4475a06e641da3ed9bdff18f9
Aaron Clark <aaron.clark@biogen.com>
2022-08-31 08:29:29 -0400

    updated link on pdf

--- print-to-pdf.R
+++ print-to-pdf.R
@@ -2,4 +2,5 @@
 
 # remotes::install_github('rstudio/pagedown')
 # install.packages("pagedown") # works fine
-pagedown::chrome_print('tracking_usage_pres.html')
+# pagedown::chrome_print('tracking_usage_pres.html')
+pagedown::chrome_print('tracking_usage_pres_crug.html')
